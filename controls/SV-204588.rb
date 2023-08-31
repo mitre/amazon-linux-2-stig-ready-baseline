7de5e9ca327d190487e6052abfcc11632a73de09
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-08-31 15:05:29 -0400

    Update SV-204588.rb for AMZN

--- controls/SV-204588.rb
+++ controls/SV-204588.rb
@@ -3,14 +3,11 @@ control 'AMZL-02-740330' do
     authentication using RSA rhosts authentication.'
   desc 'Configuring this setting for the SSH daemon provides additional assurance that remote logon via SSH will
     require a password, even in the event of misconfiguration elsewhere.'
-  desc 'check', %q(Check the version of the operating system with the following command:
-    # cat /etc/redhat-release
-    If the release is 7.4 or newer this requirement is Not Applicable.
-    Verify the SSH daemon does not allow authentication using RSA rhosts authentication.
+  desc 'check', 'Verify the SSH daemon does not allow authentication using RSA rhosts authentication.
     To determine how the SSH daemon's "RhostsRSAAuthentication" option is set, run the following command:
     # grep RhostsRSAAuthentication /etc/ssh/sshd_config
     RhostsRSAAuthentication no
-    If the value is returned as "yes", the returned line is commented out, or no output is returned, this is a finding.)
+    If the value is returned as "yes", the returned line is commented out, or no output is returned, this is a finding.)'
   desc 'fix', 'Configure the SSH daemon to not allow authentication using RSA rhosts authentication.
     Add the following line in "/etc/ssh/sshd_config", or uncomment the line and set the value to "no":
     RhostsRSAAuthentication no
@@ -26,17 +23,13 @@ control 'AMZL-02-740330' do
 
   if virtualization.system.eql?('docker') && !file('/etc/sysconfig/sshd').exist?
     impact 0.0
-    describe 'Control not applicable - SSH is not installed within containerized RHEL' do
-      skip 'Control not applicable - SSH is not installed within containerized RHEL'
+    describe 'Control not applicable - SSH is not installed within containerized AMZN' do
+      skip 'Control not applicable - SSH is not installed within containerized AMZN'
     end
-  elsif os.release.to_f >= 7.4
+  else 
     impact 0.0
-    describe "The release is #{os.release}" do
-      skip 'For RHEL 7.4 and above, this requirement is not applicable.'
-    end
-  else
-    describe sshd_config do
-      its('RhostsRSAAuthentication') { should cmp 'no' }
+    describe "Amazon Linux 2 uses the more recent version of SSH whereby RhostsRSAAuthentication is disabled by default. Not Applicable." do
+      skip 'Amazon Linux 2 uses the more recent version of SSH whereby RhostsRSAAuthentication is disabled by default. Not Applicable.'
     end
   end
 end
